f0e35129b5f94e670a3c33e10b1f8461d5bdce5c
Kylie Roenigk <kr702922@ohio.edu>
2024-05-19 23:33:01 -0400

    Started text file for quests and wrote the first draft of the hero class

---
+++ the_Hobbit.cc
@@ -0,0 +1,40 @@
+/**
+ *        @file: the_Hobbit.cc
+ *      @author: Kylie Roenigk
+ *        @date: May 19, 2024
+ *       @brief: A single player game that follows the events of the Hobbit as the Hero Travels with the Company through Middle Earth
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <vector>
+#include <string>
+using namespace std;
+
+///function prototypes
+class Hero
+{
+    string race;
+    vector <string> skills;
+    int level;
+    int exp;
+    int health;
+    vector <string> inventory;
+};
+class Adventure
+{
+     string title;
+    string description;
+    int exp_reward;
+    int damage;
+    vector <string> consequences;
+
+
+};
+
+int main(int argc, char const *argv[]) {
+
+   /*add code*/
+   return 0;
+}// main
